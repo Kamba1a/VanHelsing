c597434b4b0d7e907f9417145f0ec4b25ef263bd
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-02-01 21:40:22 +0300

    Removed CityInfoPanelBehaviour and Prefab

--- Assets/Scripts/UI/HubMap/HubMapBehaviour.cs
+++ Assets/Scripts/UI/HubMap/HubMapBehaviour.cs
@@ -1,5 +1,6 @@
 using Extensions;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,12 +13,18 @@ namespace BeastHunter
         [SerializeField] private GameObject _mainPanel;
         [SerializeField] private GameObject _infoPanel;
 
+        [Header("City info panel")]
+        [SerializeField] private GameObject _cityInfoPanel;
+        [SerializeField] private GameObject _cityName;
+        [SerializeField] private GameObject _cityDescription;
+        [SerializeField] private GameObject _citizenPanel;
+
         #endregion
 
 
         #region Fields
 
-        private GameObject _currentInfoObject;
+        private List<GameObject> _clearInfoPanelList;
 
         #endregion
 
@@ -26,8 +33,10 @@ namespace BeastHunter
 
         private void Start()
         {
+            _clearInfoPanelList = new List<GameObject>();
             _mainPanel.SetActive(Data.HubMapData.MapOnStartEnabled);
             _infoPanel.SetActive(false);
+            _cityInfoPanel.SetActive(false);
         }
 
         #endregion
@@ -47,22 +56,41 @@ namespace BeastHunter
 
         public void OnClick_CityButton(int cityId)
         {
-            Destroy(_currentInfoObject);
-
-            _currentInfoObject = GameObject.Instantiate(Data.HubMapData.CityInfoUIPrefab);
-            _currentInfoObject.transform.SetParent(_infoPanel.transform.FindDeep("Viewport"), false);
-            _currentInfoObject.transform.localScale = new Vector3(3, 1, 1);
-            _infoPanel.GetComponentInChildren<ScrollRect>().content = _currentInfoObject.GetComponent<RectTransform>();
-
-            _currentInfoObject.GetComponentInChildren<CityInfoPanelBehaviour>().Initialize(Data.HubMapData.Cities[cityId]);
-
+            ClearInfoPanel();
+            FillCityInfo(Data.HubMapData.Cities[cityId]);
+            _infoPanel.GetComponent<ScrollRect>().content = _cityInfoPanel.GetComponent<RectTransform>();
             _infoPanel.SetActive(true);
+            _cityInfoPanel.SetActive(true);
         }
 
         public void OnClick_CloseInfoButton()
         {
+            _cityInfoPanel.SetActive(false);
             _infoPanel.SetActive(false);
-            Destroy(_currentInfoObject);
+        }
+
+        private void FillCityInfo(ICityInfo city)
+        {
+            _cityName.GetComponent<Text>().text = city.Name;
+            _cityDescription.GetComponent<Text>().text = city.Description;
+
+            for (int i = 0; i < city.Citizens.Count; i++)
+            {
+                GameObject citizen = GameObject.Instantiate(Data.HubMapData.CitizenUIPrefab);
+                _clearInfoPanelList.Add(citizen);
+                citizen.transform.SetParent(_citizenPanel.transform, false);
+                citizen.transform.localScale = new Vector3(1, 1, 1);
+                citizen.GetComponentInChildren<CitizenInfoBehaviour>().Initialize(city.Citizens[i]);
+            }
+        }
+
+        private void ClearInfoPanel()
+        {
+            for (int i=0; i< _clearInfoPanelList.Count; i++)
+            {
+                Destroy(_clearInfoPanelList[i]);
+            }
+            _clearInfoPanelList.Clear();
         }
 
         #endregion
