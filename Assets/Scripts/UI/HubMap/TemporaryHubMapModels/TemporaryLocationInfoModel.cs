eb73ba1c8506bee25138b1089ced567b5abd91b5
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-02-26 14:42:46 +0300

    ILocationInfo, TemporaryLocationInfoModel

---
+++ Assets/Scripts/UI/HubMap/TemporaryHubMapModels/TemporaryLocationInfoModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace BeastHunter
+{
+    [Serializable]
+    public class TemporaryLocationInfoModel : ILocationInfo
+    {
+        #region Fields
+
+        [SerializeField] private int _id;
+        [SerializeField] private string _name;
+        [SerializeField] private string _description;
+        [SerializeField] private Sprite _screenshot;
+        [SerializeField] private int[] _dwellersId;
+        [SerializeField] private int[] _ingredientsId;
+
+        #endregion
+
+
+        #region Properties
+
+        public int Id => _id;
+        public string Name => _name;
+        public string Description => _description;
+        public Sprite Screenshot => _screenshot;
+        public int[] DwellersId => _dwellersId;
+        public int[] IngredientsId => _ingredientsId;
+
+        #endregion
+    }
+}
