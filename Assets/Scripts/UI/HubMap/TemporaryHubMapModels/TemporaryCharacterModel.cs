349a8b9dc89506399f35f58b74e517eaff70514d
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-02-27 19:09:50 +0300

    ICharacter, TemporaryCharacterModel

---
+++ Assets/Scripts/UI/HubMap/TemporaryHubMapModels/TemporaryCharacterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace BeastHunter
+{
+    [Serializable]
+    public class TemporaryCharacterModel : ICharacter
+    {
+        #region Fields
+
+        [SerializeField] private int _id;
+        [SerializeField] private Sprite _portrait;
+        [SerializeField] private int?[] _itemsId;
+
+        #endregion
+
+
+        #region Properties
+
+        public int Id => _id;
+        public Sprite Portrait => _portrait;
+        public int?[] ItemsId => _itemsId;
+
+        #endregion
+    }
+}
