13c1c1435aee5383abef0f46c9edc7aeae746f93
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-03-08 15:57:11 +0300

    Rename classes

---
+++ Assets/Scripts/UI/HubMap/HubMapBehaviours/EquipmentItemHubMapUIBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeastHunter
+{
+    class EquipmentItemHubMapUIBehaviour: MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private Image _itemImage;
+        [SerializeField] private Button _itemButton;
+
+        #endregion
+
+
+        #region Properties
+
+        public int? CurrentItemId { get; private set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public void SetInteractable(bool flag)
+        {
+            _itemButton.interactable = flag;
+        }
+
+        public void PutItemInCell(IItemInfo item)
+        {
+            CurrentItemId = item.Id;
+            SetImage(item.Image);
+        }
+
+        public void ClearCell()
+        {
+            CurrentItemId = null;
+            SetImage(null);
+        }
+
+        private void SetImage(Sprite image)
+        {
+            if (image != null)
+            {
+                Color color = _itemImage.color;
+                color.a = 255f;
+                _itemImage.color = color;
+            }
+            else
+            {
+                Color color = _itemImage.color;
+                color.a = 0f;
+                _itemImage.color = color;
+            }
+            _itemImage.sprite = image;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/HubMap/HubMapBehaviours/SellingItemHubMapUIBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeastHunter
+{
+    class SellingItemHubMapUIBehaviour: MonoBehaviour
+    {
+        [SerializeField] private Image _itemImage;
+        [SerializeField] private GameObject _itemNotSellingImage;
+
+        public void Initialize(IItemInfo item, bool isAvailableForSale)
+        {
+            _itemImage.sprite = item.Image;
+            SetAvailability(isAvailableForSale);
+        }
+
+        public void SetAvailability(bool isAvailableForSale)
+        {
+            _itemNotSellingImage.SetActive(!isAvailableForSale);
+        }
+    }
+}
