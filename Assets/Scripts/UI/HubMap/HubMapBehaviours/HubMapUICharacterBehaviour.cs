f979c8c6d1b81f6a406cdf8802ca67474ea44c10
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-03-09 19:50:35 +0300

    Renamed behaviour classes

---
+++ Assets/Scripts/UI/HubMap/HubMapBehaviours/HubMapUICharacterBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeastHunter
+{
+    public class HubMapUICharacterBehaviour: MonoBehaviour
+    {
+        #region Properties
+
+        public Action<int[]> OnClick_CharacterButtonHandler { get; set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public void Initialize(IHubMapUICharacter character)
+        {
+            GetComponent<Image>().sprite = character.Portrait;
+            GetComponent<Button>().onClick.AddListener(() => OnClick_CharacterButton(character.ItemsId));
+        }
+
+        public void OnClick_CharacterButton(int[] items)
+        {
+            OnClick_CharacterButtonHandler?.Invoke(items);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/HubMap/HubMapBehaviours/HubMapUISellingItemBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeastHunter
+{
+    class HubMapUISellingItemBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Image _itemImage;
+        [SerializeField] private GameObject _itemNotSellingImage;
+
+        public void Initialize(IHubMapUIItem item, bool isAvailableForSale)
+        {
+            _itemImage.sprite = item.Image;
+            SetAvailability(isAvailableForSale);
+        }
+
+        public void SetAvailability(bool isAvailableForSale)
+        {
+            _itemNotSellingImage.SetActive(!isAvailableForSale);
+        }
+    }
+}
