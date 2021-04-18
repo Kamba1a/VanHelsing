b7510107bac97ea892a5575f56d3446c517ec7b4
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-04-18 11:29:37 +0300

    Added weapon items (model, data, SO)

---
+++ Assets/Scripts/UI/HubMap/HubMapUIItems/HubMapUIWeaponItemData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace BeastHunter
+{
+    [CreateAssetMenu(fileName = "HubMapUIWeaponItemData", menuName = "CreateData/HubMapUIData/Items/Weapon", order = 0)]
+    public class HubMapUIWeaponItemData : HubMapUIBaseItemData
+    {
+        [SerializeField] private bool _isTwoHanded;
+
+
+        public bool IsTwoHanded => _isTwoHanded;
+        public override HubMapUIItemType ItemType { get; protected set; }
+
+
+        private void OnEnable()
+        {
+            ItemType = HubMapUIItemType.Weapon;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HubMap/HubMapUIItems/HubMapUIWeaponItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BeastHunter
+{
+    public class HubMapUIWeaponItemModel : HubMapUIBaseItemModel
+    {
+        public bool IsTwoHanded { get; private set; }
+
+        public HubMapUIWeaponItemModel(HubMapUIWeaponItemData data) : base(data)
+        {
+            IsTwoHanded = data.IsTwoHanded;
+        }
+    }
+}
