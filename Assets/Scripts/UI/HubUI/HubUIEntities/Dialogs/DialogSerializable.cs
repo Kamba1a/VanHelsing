9453a6d925854e8a680b312b4b6e0c20e0d79f71
Kamba1a <46526582+Kamba1a@users.noreply.github.com>
2021-06-09 14:02:47 +0300

    DialogsPool

---
+++ Assets/Scripts/UI/HubUI/HubUIEntities/Dialogs/DialogSerializable.cs
@@ -0,0 +1,9 @@
+﻿namespace BeastHunterHubUI
+{
+    [System.Serializable]
+    public class DialogSerializable
+    {
+        public CitizenData CitizenData;
+        public CitizenDialog CitizenDialog;
+    }
+}
